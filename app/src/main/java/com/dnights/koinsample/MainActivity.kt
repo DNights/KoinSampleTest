0fe975dd8c6d574f781a7552544528ccd11250a3
hyundonghan <guehgk456@gmail.com>
2019-07-11 18:37:44 +0900

    add service in MainActivity

--- app/src/main/java/com/dnights/koinsample/MainActivity.kt
+++ app/src/main/java/com/dnights/koinsample/MainActivity.kt
@@ -2,11 +2,22 @@ package com.dnights.koinsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dnights.koinsample.app.BusinessService
+import com.dnights.koinsample.di.myModule
+import org.koin.android.ext.android.get
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    // lazy inject BusinessService into property
+    val service : BusinessService by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // or directly get any instance
+        val service2 : BusinessService = get()
+
     }
 }
