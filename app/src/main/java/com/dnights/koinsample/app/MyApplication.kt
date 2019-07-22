ad38bb768db0e95769d7bbc4a5fb7ee0f748a69f
hyundonghan <guehgk456@gmail.com>
2019-07-22 16:30:55 +0900

    add stopKoin() in MyApplication.kt

--- app/src/main/java/com/dnights/koinsample/app/MyApplication.kt
+++ app/src/main/java/com/dnights/koinsample/app/MyApplication.kt
@@ -4,11 +4,14 @@ import android.app.Application
 import com.dnights.koinsample.di.myModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 
 
 class MyApplication : Application() {
     override fun onCreate(){
         super.onCreate()
+
+        stopKoin()
         // start Koin!
         startKoin {
             // declare used Android context
@@ -16,5 +19,10 @@ class MyApplication : Application() {
             // declare modules
             modules(myModule)
         }
+    }
+
+    override fun onTerminate() {
+        stopKoin()
+        super.onTerminate()
     }
 }
