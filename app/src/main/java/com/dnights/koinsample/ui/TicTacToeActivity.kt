ac1acb178c837f717d0e7bacfd2c05d5560eefb5
hyundonghan <guehgk456@gmail.com>
2019-08-02 00:10:31 +0900

    edited MVC -> MVVM

--- app/src/main/java/com/dnights/koinsample/ui/TicTacToeActivity.kt
+++ app/src/main/java/com/dnights/koinsample/ui/TicTacToeActivity.kt
@@ -1,34 +1,38 @@
 package com.dnights.koinsample.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.dnights.koinsample.R
-import com.dnights.koinsample.model.Board
+import com.dnights.koinsample.databinding.TictactoeBinding
+import com.dnights.koinsample.viewmodel.TicTacToeViewModel
+
 
 class TicTacToeActivity : BaseActivity(){
 
-    private val TAG = TicTacToeActivity::class.java.name
-
-    private var model: Board? = null
-
-    private var buttonGrid: ViewGroup? = null
-    private var winnerPlayerViewGroup: View? = null
-    private var winnerPlayerLabel: TextView? = null
+    private var viewModel = TicTacToeViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tictactoe)
-        winnerPlayerLabel = findViewById(R.id.winnerPlayerLabel) as TextView
-        winnerPlayerViewGroup = findViewById(R.id.winnerPlayerViewGroup)
-        buttonGrid = findViewById(R.id.buttonGrid) as ViewGroup
+        val binding = DataBindingUtil.setContentView(this, R.layout.tictactoe) as TictactoeBinding
+        binding.viewModel = viewModel
+        viewModel.onCreate()
+    }
 
-        model = Board()
+    override fun onPause() {
+        super.onPause()
+        viewModel.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.onDestroy()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -40,42 +44,10 @@ class TicTacToeActivity : BaseActivity(){
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_reset -> {
-                reset()
+                viewModel.onResetSelected()
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    fun onCellClicked(v: View) {
-
-        val button = v as Button
-
-        val tag = button.tag.toString()
-        val row = Integer.valueOf(tag.substring(0, 1))
-        val col = Integer.valueOf(tag.substring(1, 2))
-        Log.i(TAG, "Click Row: [$row,$col]")
-
-        val playerThatMoved = model!!.mark(row, col)
-
-        if (playerThatMoved != null) {
-            button.setText(playerThatMoved!!.toString())
-            if (model!!.getWinner() != null) {
-                winnerPlayerLabel!!.setText(playerThatMoved!!.toString())
-                winnerPlayerViewGroup!!.visibility = View.VISIBLE
-            }
-        }
-
-    }
-
-    private fun reset() {
-        winnerPlayerViewGroup!!.visibility = View.GONE
-        winnerPlayerLabel!!.text = ""
-
-        model!!.restart()
-
-        for (i in 0 until buttonGrid!!.childCount) {
-            (buttonGrid!!.getChildAt(i) as Button).text = ""
         }
     }
 }
